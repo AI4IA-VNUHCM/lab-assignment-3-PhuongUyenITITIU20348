a90d53241ce1cac368679e3a0911d7123a019cef
PhuongUyenITITIU20348 <ititiu20348@student.hcmiu.edu.vn>
2021-10-18 17:04:05 +0700

    ex5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -13,7 +13,31 @@ ______________________________________
 
 void Ex5(int arr[], int n){
 	//Your codes here
-	
+	int positive[n];
+    int negative[n];
+    int p_count = 0;
+    int n_count = 0;
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] >= 0){
+            positive[p_count] = arr[i];
+            p_count +=1;
+        }else{
+            negative[n_count] = arr[i];
+            n_count +=1;
+        }
+    }
+
+    for(int i = 0; i < p_count; i++){
+        if(i == 0){
+            printf("%d", positive[i]);
+        }else{
+            printf(" %d", positive[i]);
+        }
+    }
+    for(int i = 0; i < n_count; i++){
+        printf(" %d", negative[i]);
+    }
 }
 
 int main(int argc, char *argv[]) {
